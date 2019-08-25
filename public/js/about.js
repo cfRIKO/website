f4ff6f204e199b990113b2968fddcbce6ac420e3
cfRIKO <1g.rikoje@gmail.com>
2019-08-25 13:20:38 +0200

    Adding functonality to the buttons on about.html and changing style for the main box!

---
+++ public/js/about.js
@@ -0,0 +1,9 @@
+var button = document.getElementById('projects')
+button.addEventListener('click', (event) => {
+    window.location.href = 'projects.html'
+})
+
+var button = document.getElementById('contact')
+button.addEventListener('click', (event) => {
+    window.location.href = 'contact.html'
+})
