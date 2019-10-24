3d559426a91314c356519fdbe8263f5ac3a4e56f
cfRIKO <1g.rikoje@gmail.com>
2019-10-24 17:00:48 +0200

    Added styles and support for resizing. Icons are now sized according to the screen height. Link to git repos changed.

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,14 +1,19 @@
-if (document.documentElement.clientWidth > 480) {
-  menuitems = document.getElementsByClassName('menuitem')
-  document.getElementById('togglemenu').classList.toggle('shownmenu')
-
-  for (let index = 0; index < menuitems.length; index++) {
-    menuitems[index].classList.toggle('show')
-  }  
-} else {
-  document.getElementById('togglemenu').classList.toggle('hiddenmenu')
+resize = () => {
+  if (document.documentElement.clientWidth > 480) {
+    menuitems = document.getElementsByClassName('menuitem')
+    document.getElementById('togglemenu').classList.value = 'shownmenu'
+  
+    for (let index = 0; index < menuitems.length; index++) {
+      menuitems[index].classList.toggle('show')
+    }  
+  } else {
+    document.getElementById('togglemenu').classList.value = 'hiddenmenu'
+  }
+  icons = document.getElementsByClassName('material-icons')
+  for (let index = 0; index < icons.length; index++) {
+    document.documentElement.clientHeight <= 480 ? icons[index].classList.add('smallicon') : console.log(icons[index].classList.remove('smallicon')) 
+  }
 }
-
 
 showMenu = () => {
   menuitems = document.getElementsByClassName('menuitem')
@@ -19,3 +24,7 @@ showMenu = () => {
     menuitems[index].classList.toggle('show')
   }
 }
+
+resize()
+
+window.onresize = resize
